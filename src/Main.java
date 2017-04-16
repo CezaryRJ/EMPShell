6d5af97725cecae73ce8231e1d80406174725142
zazzy13 <cezaryrj@ulrik.uio.no>
2017-04-16 21:29:35 +0200

    Added a bunch of commands

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -11,10 +10,10 @@ public class Main {
 
 		System.out.println("Welcome to EMPShell");
 		String currentPath = System.getProperty("user.dir");
-		Timer timer = new Timer();
+		
 		Scanner inn;
 		Manager manager = new Manager();
-		timer.start();
+		
 		final int coreCount = Runtime.getRuntime().availableProcessors();
 
 		try {
@@ -33,57 +32,10 @@ public class Main {
 		} catch (FileNotFoundException e) {
 			// scan all subfolders on first boot
 
-			PrintWriter writer = new PrintWriter("files.txt", "UTF-8");
-			File[] listOfFiles = new File(currentPath).listFiles();
-
-			ArrayList<ArrayList<String>> cache = new ArrayList<>();
-			cache.add(new ArrayList<String>());
-
-			Thread[] crawler = new Thread[coreCount];
-
-			File tmp;
-
-			for (int i = 0; i < listOfFiles.length; i++) {
-				tmp = new File(listOfFiles[i].getPath());
-
-				if (tmp.isDirectory()) {
-					for (int x = 0; x < crawler.length; x++) {
-						if (crawler[x] == null || !crawler[x].isAlive()) {
-							cache.add(new ArrayList<String>());
-							crawler[x] = new Thread(new Crawler(tmp.getAbsolutePath(), cache.get(cache.size() - 1)));
-							crawler[x].start();
-							break;
-						}
-					}
-
-				} else {
-					cache.get(0).add(tmp.getAbsolutePath());
-				}
-
-			}
-			for (int i = 0; i < crawler.length; i++) {
-				if (crawler[i] != null) {
-					crawler[i].join();
-
-				}
-
-			}
-			int counter = 0;
-			for (int i = 0; i < cache.size(); i++) {
-				ArrayList<String> tmpArr = cache.get(i);
-				for (int x = 0; x < cache.get(i).size(); x++) {
-					writer.println(tmpArr.get(x));
-					counter++;
-
-				}
-
-			}
-			writer.close();
-			System.out.print(counter + " files gathered in ");
+			manager.indexer(currentPath);
 
 		}
-		timer.stop();
-
+		
 		ArrayList<String> tokens = new ArrayList<>();
 		inn = new Scanner(System.in);
 		while (true) {
@@ -92,8 +44,7 @@ public class Main {
 			if (tokens.get(0).equals("ls")) {
 				manager.listFiles(currentPath);
 
-			}
-			if (tokens.get(0).equals("cd")) {
+			} else if (tokens.get(0).equals("cd")) {
 				if (tokens.size() == 1) {
 					currentPath = manager.exitFolder(currentPath);
 				} else {
@@ -101,7 +52,42 @@ public class Main {
 					currentPath = manager.enterFolder(currentPath, tokens.get(1));
 				}
 
-			} else if (tokens.get(0).equals("exit")) {
+			} else if (tokens.get(0).equals("exe")) {
+				manager.open(currentPath, tokens);
+
+			} else if (tokens.get(0).equals("help")) {
+				manager.help();
+			} else if (tokens.get(0).equals("goto")) {
+				if (tokens.size() == 1) {
+					String tmp = manager.goToc();
+					if (new File(tmp).isDirectory()) {
+						currentPath = manager.goToc();
+						System.out.println(currentPath);
+					} else {
+						System.out.println("No such directory");
+					}
+
+				} else {
+					if (new File(tokens.get(1)).exists()) {
+						currentPath = tokens.get(1);
+						System.out.println(currentPath);
+
+					} else {
+						System.out.println("No such directory");
+					}
+				}
+			}
+			else if(tokens.get(0).equals("index")){
+				manager.indexer(currentPath);
+			}
+			else if (tokens.get(0).equals("delete")){
+				manager.delete(currentPath,tokens);
+			}
+			else if (tokens.get(0).equals("opendir")){
+				manager.openFolder(currentPath);
+			}
+
+			else if (tokens.get(0).equals("exit")) {
 				System.exit(0);
 			}
 
--- src/Manager.java
+++ src/Manager.java
@@ -1,27 +1,141 @@
+import java.awt.Desktop;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
 import java.io.File;
-import java.nio.file.Files;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Manager {
+	
+	public void openFolder(String currentPath) throws IOException{
+		Desktop.getDesktop().open(new File(currentPath));
+		
+	}
+	
+	public void delete(String path,ArrayList<String> args){
+		
+		File tmp;
+		for(int i = 1;i<args.size();i++){
+			
+			tmp = new File(path + "\\" + args.get(i));
+			if(tmp.delete()){
+				System.out.println("File " + tmp.getName() + " has been deleted");
+			}
+			else {
+				System.out.println("File " + tmp.getName() + " could not be deleted");
+			}
+		}
+	}
+
+	public void indexer(String currentPath) throws Exception {
+
+		Timer timer = new Timer();
+		timer.start();
+		PrintWriter writer = new PrintWriter(currentPath + ".txt", "UTF-8");
+		File[] listOfFiles = new File(currentPath).listFiles();
+
+		ArrayList<ArrayList<String>> cache = new ArrayList<>();
+		cache.add(new ArrayList<String>());
+
+		Thread[] crawler = new Thread[Runtime.getRuntime().availableProcessors()];
+
+		File tmp;
+		int threads = 0;
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			tmp = new File(listOfFiles[i].getPath());
+
+			if (tmp.isDirectory()) {
+				for (int x = 0; x < crawler.length; x++) {
+					if (crawler[x] == null || !crawler[x].isAlive()) {
+						cache.add(new ArrayList<String>());
+						crawler[x] = new Thread(new Crawler(tmp.getAbsolutePath(), cache.get(cache.size() - 1)));
+						crawler[x].start();
+						threads++;
+						break;
+					}
+				}
+
+			} else {
+				cache.get(0).add(tmp.getAbsolutePath());
+			}
+
+		}
+		for (int i = 0; i < crawler.length; i++) {
+			if (crawler[i] != null) {
+				crawler[i].join();
+
+			}
+
+		}
+		int counter = 0;
+		for (int i = 0; i < cache.size(); i++) {
+			ArrayList<String> tmpArr = cache.get(i);
+			for (int x = 0; x < cache.get(i).size(); x++) {
+				writer.println(tmpArr.get(x));
+				counter++;
+
+			}
+
+		}
+		writer.close();
+		System.out.print(counter + " files gathered in ");
+		timer.stop();
+		System.out.println("\n" + threads + " threads have been used for this task");
+
+	}
+
+	public String goToc() {
+
+		try {
+			return (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+		} catch (Exception e) {
+			System.out.println("No such directory");
+		}
+		return null;
+	}
+
+	public void help() {
+		System.out.println("cd -foldername  |enter folder \ncd  " + "| exit folder\nls "
+				+ "|lists all files and subfolder in current folder\nexe -argument "
+				+ "|opens or executes the requested argument");
+	}
+
+	public void open(String path, ArrayList<String> args) {
+
+		for (int i = 1; i < args.size(); i++) {
+
+			try {
+				if (new File(path + "\\" + args.get(i)).exists()) {
+					Desktop.getDesktop().open(new File(path + "\\" + args.get(i)));
+				} else {
+					System.out.println("Failed to open file " + path + "\\" + args.get(i) + " file does not exist");
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 
 	public ArrayList<String> tokenize(String inn, String limiter) {
-	
+
 		ArrayList<String> out = new ArrayList<>();
 		int counter = 0;
-	
-		for (int i = 0; i < inn.length()-1; i++) {
+
+		for (int i = 0; i < inn.length() - 1; i++) {
 
 			if (inn.substring(i, (i + 1)).equals(limiter)) {
 
 				out.add(inn.substring(counter, i));
-				counter = (i+1);
+				counter = (i + 1);
 			}
 		}
-		out.add(inn.substring(counter , inn.length()));
-		
-		
+		out.add(inn.substring(counter, inn.length()));
+
 		return out;
 	}
 
@@ -52,25 +166,25 @@ public class Manager {
 	public String enterFolder(String oldPath, String fileName) {
 
 		System.out.println(oldPath + "\\" + fileName);
-		if(new File(oldPath + "\\" + fileName).exists()){
-			return  oldPath + "\\" + fileName;
-		}
-		else {
+		if (new File(oldPath + "\\" + fileName).exists()) {
+			return oldPath + "\\" + fileName;
+		} else {
 			System.out.println("Invalid path name");
 			return oldPath;
 		}
 
 	}
-	public String exitFolder(String path){
-		
-	ArrayList<String> tmp = tokenize(path,"\\");
-		
-	String out = tmp.get(0);
-		for(int i = 1; i<tmp.size()-1;i++){
-			out = out +"\\" + tmp.get(i);
+
+	public String exitFolder(String path) {
+
+		ArrayList<String> tmp = tokenize(path, "\\");
+
+		String out = tmp.get(0);
+		for (int i = 1; i < tmp.size() - 1; i++) {
+			out = out + "\\" + tmp.get(i);
 		}
 		System.out.println(out);
 		return out;
 	}
-	
+
 }
