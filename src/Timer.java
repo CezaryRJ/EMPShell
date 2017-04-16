4b94c7dbb453bc24236b5853e077e35ae237fef0
zazzy13 <cezaryrj@ulrik.uio.no>
2017-04-16 21:29:35 +0200

    Added a bunch of commands

--- src/Timer.java
+++ src/Timer.java
@@ -2,15 +2,14 @@
 public class Timer {
 
 	long start = 0;
-	
-	
-	public void start(){
+
+	public void start() {
 		start = System.nanoTime();
 	}
-	public void stop(){
-	
+
+	public void stop() {
+
 		System.out.print((double) (System.nanoTime() - start) / 1000000 + " miliseconds");
 	}
-	
-	
+
 }
