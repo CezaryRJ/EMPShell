d9142fa128b807d54160a8285a723be7b5b55c46
zazzy13 <cezaryrj@ulrik.uio.no>
2017-05-16 00:49:03 +0200

    Added recorder for command history

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,11 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class DataBase {
-	HashMap<String, Boolean> database = new HashMap<String, Boolean>();
+	HashMap<String, Boolean> audio = new HashMap<String, Boolean>();
+	HashMap<String, Boolean> image = new HashMap<String, Boolean>();
+	HashMap<String, Boolean> other = new HashMap<String, Boolean>();
 
 	public void readData(String file) {
 		Scanner inn = new Scanner(new File(file));
@@ -25,10 +28,7 @@ public class DataBase {
 		} catch (FileNotFoundException e) {
 			// scan all subfolders on first boot, or if db file is not found
 
-			tmpArr = new ArrayList<>();
-			tmpArr.add(currentPath);
-			manager.runVoid.get("index").run(tmpArr);
-
+			index(System.getProperty("user.dir"));
 		}
 
 		/*
@@ -55,6 +55,133 @@ public class DataBase {
 		 * 
 		 * }
 		 */
+	}
+
+	public void index(String path) throws Exception {
+
+		Timer timer = new Timer();
+		timer.start();
+		// must cut of first directory
+
+		ArrayList<String> pathTokens = tokenize(path, "\\");
+
+		String out = pathTokens.get(0);
+		for (int i = 1; i < pathTokens.size(); i++) {
+			out = out + "\\" + pathTokens.get(i);
+		}
+
+		System.out.println(out + "\\empdb.txt");
+		PrintWriter writer = new PrintWriter(out + "\\empdb.txt", "UTF-8");
+
+		File[] listOfFiles = new File(path).listFiles();
+
+		ArrayList<ArrayList<ArrayList<String>>> cache = new ArrayList<>();
+		cache.add(new ArrayList<ArrayList<String>>());
+		for (int y = 0; y < 3; y++) {
+			cache.get(cache.size() - 1).add(new ArrayList<String>());
+		}
+		Crawler thisFolder = new Crawler(path, cache.get(0));
+
+		Thread[] crawler = new Thread[Runtime.getRuntime().availableProcessors()];
+
+		File tmp;
+		int threads = 0;
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			tmp = new File(listOfFiles[i].getPath());
+
+			if (tmp.isDirectory()) {
+				for (int x = 0; x < crawler.length; x++) {
+
+					if (crawler[x] == null || !crawler[x].isAlive()) {
+						cache.add(new ArrayList<ArrayList<String>>());
+						for (int y = 0; y < 3; y++) {
+							cache.get(cache.size() - 1).add(new ArrayList<String>());
+						}
+						crawler[x] = new Thread(new Crawler(tmp.getAbsolutePath(), cache.get(cache.size() - 1)));
+						crawler[x].start();
+						threads++;
+						break;
+					}
+				}
+
+			} else {
+				// denne sjekke filene i root mappen
+				thisFolder.classifier.classify(tmp.getAbsolutePath());
+			}
+
+		}
+		for (int i = 0; i < crawler.length; i++) {
+			if (crawler[i] != null) {
+
+				crawler[i].join();
+
+			}
+
+		}
+
+		int counter = 0;
+		int fileCounter = 0;
+		// legg sammen resultater
+
+		for (int i = 0; i < 3; i++) {
+
+			for (int x = 0; x < cache.size(); x++) {
+				counter += cache.get(x).get(i).size();
+
+			}
+			System.out.println(counter);
+			writer.println(counter);
+
+			for (int x = 0; x < cache.size(); x++) {
+				for (int y = 0; y < cache.get(x).get(i).size(); y++) {
+					writer.println(cache.get(x).get(i).get(y));
+
+				}
+
+			}
+
+			fileCounter += counter;
+			counter = 0;
+		}
+
+		/*
+		 * for (int b = 0; b < cache.size(); b++) { for (int c = 0; c <
+		 * cache.get(b).size(); c++) { for (int d = 0; d <
+		 * cache.get(b).get(c).size(); d++) {
+		 * writer.println(cache.get(b).get(c).get(d)); counter++; } }
+		 * 
+		 * counter = 0;
+		 * 
+		 * }
+		 */
+		writer.print(fileCounter);
+		writer.close();
+		System.out.print(fileCounter + " files gathered in ");
+		timer.stop();
+		System.out.println("\n" + threads + " threads have been used for this task");
+
+	}
+	public void writeToFile(){
+		
+	}
+
+	public ArrayList<String> tokenize(String inn, String limiter) {
+
+		ArrayList<String> out = new ArrayList<>();
+		int counter = 0;
+
+		for (int i = 0; i < inn.length() - 1; i++) {
+
+			if (inn.substring(i, (i + 1)).equals(limiter)) {
+
+				out.add(inn.substring(counter, i));
+				counter = (i + 1);
+			}
+		}
+		out.add(inn.substring(counter, inn.length()));
+
+		return out;
 	}
 
 }
