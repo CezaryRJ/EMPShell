3b75a78a596e454b9d539bed8105b98f26edc8d6
zazzy13 <cezaryrj@ulrik.uio.no>
2017-05-19 17:37:39 +0200

    Remade party mixer to pick files from the database

--- src/PartyMixer.java
+++ src/PartyMixer.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Makes a playlist consisting of the requested amount of tracks from the databse
+ * For some reason it does not work with not ascii filenames
  * 
  * @author Cezary
  *
@@ -25,17 +27,22 @@ public class PartyMixer implements runVoid {
 		ArrayList<String> tokens;
 
 		int filesWanted = Integer.parseInt(inn.get(0));
-		int filesFound = 0;
-
+		
 		PrintWriter writer = new PrintWriter(playlist);
 		writer.println("#EXTM3U");
 
-		for (int i = 0; i < fileList.length && filesFound != filesWanted; i++) {
-			tokens = Manager.tokenize(fileList[i].getName(), ".");
+		
+		Random random = new Random();
+		ArrayList<String> keys  = new ArrayList<String>(manager.database.getGroup(0).keySet());
+		String randomKey;
+		
+		for (int i = 0; i <  filesWanted; i++) {
+			randomKey = keys.get( random.nextInt(keys.size()) );
+			tokens = Manager.tokenize(randomKey, ".");
 			if (tokens.get(tokens.size() - 1).equals("mp3")) {
-				writer.println("#EXTINF:0," + fileList[i].getName());
-				writer.println(fileList[i].getAbsolutePath());
-				filesFound++;
+				writer.println("#EXTINF:0," + randomKey);
+				writer.println(randomKey);
+			
 			}
 
 		}
