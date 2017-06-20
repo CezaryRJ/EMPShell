4d9ac3cf79a38725165326033ae851183efdcb36
zazzy13 <cezaryrj@ulrik.uio.no>
2017-06-21 01:12:02 +0200

    Fixed nullpointers while indexing

--- src/indexing/Posting.java
+++ src/indexing/Posting.java
@@ -3,8 +3,7 @@ package indexing;
 public class Posting {
 
 	private int docID;
-	private String path;
-	private String type;
+	
 	
 	public Posting(int docID){
 		this.setDocID(docID);
@@ -18,19 +17,5 @@ public class Posting {
 		this.docID = docID;
 	}
 
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
+	
 }
